ba006342ed7d4e1c8daf23270a24e15a1b5f7e0e
Zeona Walker-Latney <zeonawalker-latney@Zeonas-MBP.fios-router.home>
2022-10-01 11:53:19 -0400

    fixing error

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR,"STATIC")]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 else:
-    STATIC_ROOT = [os.path.join(BASE_DIR,"STATIC")]
+    STATIC_ROOT = [os.path.join(BASE_DIR,"static")]
 
 STATIC_URL = '/static/'
 
